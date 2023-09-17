277116453881cbf1b339cb71d9c32527534c27e6
Owen Taylor <Owentaylor60@yahoo.com>
2023-09-17 10:35:42 -0400

    testing

---
+++ client/build/precache-manifest.8b580041e5244f7105060ce7d3c65546.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffc9c26abb02905eae939d46b835b36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdf551418e2213249e06",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "cec733f26b4246ed8f2f",
+    "url": "/static/js/2.a47e473a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a47e473a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf551418e2213249e06",
+    "url": "/static/js/main.034fd0bf.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.034fd0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
-
+import { useStoreContext } from '../../utils/GlobalState'
 function CategoryMenu({ setCategory }) {
+  const [state,dispatch] = useStoreContext();
+  const { categories } = state;
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
-
+ useEffect(() => {
+if(categoryData) {
+  dispatch({
+    type: UPDATE_CATEGORIES,
+    categories: categoryData.categories
+  });
+}
+}, [categoryData,dispatch]);
   return (
     <div>
       <h2>Choose a Category:</h2>
